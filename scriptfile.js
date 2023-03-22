d075d57a293725463d5086ec2dddcd70668e4f6b
DebFullstack <debbieking1979@gmail.com>
2023-03-21 23:48:22 -0400

    <adding basic code to all files and adding an img file>

--- scriptfile.js
+++ scriptfile.js
@@ -1,1 +1,9 @@
+// 1. Create a string for the user that reads: You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39
+// 2. Assign 3 variables. Each variable contains the corresponding result of a calculation using a unique arithmetic operator (+ - * /) Each must = one of the 3 codes of the combination 10 - 40 - 39
+// 3. Create a dialogue box displaying the vault code and the text if using html anda script tag, or creat a popup dialog.
+
+const comb1 = 1 * 10; //expected 10
+const comb2 = 80 / 2; //expected 40
+const comb3 = 40 - 1; //expected 39
+alert("You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: " + comb1 + "-" + comb2 + "-" + comb3)
 
